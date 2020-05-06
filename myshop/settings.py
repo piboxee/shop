cf14822ae4285cf0b4427ac016a1f0c23a08e12a
Valentin Piho <pibox.ee@gmail.com>
2020-05-06 15:07:15 +0300

    Cart + Orders

--- myshop/settings.py
+++ myshop/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -82,6 +86,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+CART_SESSION_ID = 'cart'
 
 
 # Password validation
--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
 
+from cart.forms import CartAddProductForm
+
 
 def product_list(request, category_slug=None):
     category = None
@@ -24,8 +26,10 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
 
     context = {
         'product': product,
+        'cart_product_form': cart_product_form,
     }
     return render(request, 'shop/product/detail.html', context)
